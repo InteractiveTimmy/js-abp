b7b02680ecca881f99dd1a930f54719223286a7e
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-03-30 19:13:49 -0400

    add rollup for testing

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+export default {
+	input: 'dist/index.js',
+	output: [
+		{
+			format: 'umd',
+			name: 'JSABP',
+			file: 'build/js-abp.js',
+			indent: '\t'
+		},
+		{
+			format: 'es',
+			file: 'build/js-abp.module.js',
+			indent: '\t'
+		}
+	]
+};
